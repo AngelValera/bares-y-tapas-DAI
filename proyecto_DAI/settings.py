31d2c51fc8beb99432ba60ae39d23ea5a897868e
AngelValera <angelvalera.epal@gmail.com>
2016-02-06 18:29:06 +0100

    arreglo css

--- proyecto_DAI/settings.py
+++ proyecto_DAI/settings.py
@@ -119,9 +119,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR,'static')
 
-STATICFILES_DIRS = (
-    STATIC_PATH,
-)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
