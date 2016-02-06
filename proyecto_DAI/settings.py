70675a3f1a8ff2dcadf8b7b93cfc1aec6f90d74d
AngelValera <angelvalera.epal@gmail.com>
2016-02-06 18:12:35 +0100

    añado whitenoise

--- proyecto_DAI/settings.py
+++ proyecto_DAI/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATIC_PATH = os.path.join(BASE_DIR,'static')
