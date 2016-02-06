a6d8f022e6d00bb1172b6c48f35477b7cb9ad771
AngelValera <angelvalera.epal@gmail.com>
2016-02-06 18:45:48 +0100

    arreglo css

--- proyecto_DAI/settings.py
+++ proyecto_DAI/settings.py
@@ -112,20 +112,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-#STATIC_URL = '/static/'
-#STATIC_PATH = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_PATH = os.path.join(BASE_DIR,'static')
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    STATIC_PATH,
 )
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
