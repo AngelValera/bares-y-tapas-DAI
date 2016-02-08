a66ae740abe3d31bd9fe49b6205edb3a2036944c
AngelValera <angelvalera.epal@gmail.com>
2016-02-08 18:55:37 +0100

    resubiendo documentaccion de vagrant ansible y fabric

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import task, run, local, hosts, cd, env
 
 #  Ejecutamos la aplicacion
 def ejecutar_app():
-    run('sudo  python /root/bares-y-tapas-DAI/manage.py runserver 0.0.0.0:80')
+    run('sudo  python /root/bares-y-tapas-DAI/manage.py runserver 0.0.0.0:80 &')
 
 #  Borramos la aplicacion
 def borrar():
@@ -11,4 +11,4 @@ def borrar():
 
 # Realizar  Test
 def test():
-	run('sudo  python /root/bares-y-tapas-DAI/manage.py test ')    
+	run('sudo  python /root/bares-y-tapas-DAI/manage.py test ')
