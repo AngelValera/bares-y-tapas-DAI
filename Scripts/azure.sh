51d6db6d0209751b7a05eb7b75704999a5cc45ff
AngelValera <angelvalera.epal@gmail.com>
2016-02-08 19:35:49 +0100

    arreglo script de azure.sh

--- Scripts/azure.sh
+++ Scripts/azure.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #Despliegue en Azure usando vagrant
+cd ../despliegueAzure
 sudo vagrant up --provider=azure
+cd ..
 sudo fab -p 'Clave#Angel#1' -H vagrant@maquinaavm-service-xuybo.cloudapp.net ejecutar_app
