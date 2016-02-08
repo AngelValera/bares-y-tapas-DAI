35239c6494fa8527ed5656a353d257656feb30c8
AngelValera <angelvalera.epal@gmail.com>
2016-02-08 17:48:52 +0100

    subiendo fabfile

--- Scripts/azure.sh
+++ Scripts/azure.sh
@@ -1,3 +1,4 @@
 #Despliegue en Azure usando vagrant
 cd ../despliegueAzure/
 vagrant up --provider=azure
+fab -p 'Clave#Angel#1' -H vagrant@maquinaavm-service-xuybo.cloudapp.net ejecutar_app
---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import task, run, local, hosts, cd, env
+
+
+########   Ejecutamos la aplicacion
+def ejecutar_app():
+    run('sudo  python /root/bares-y-tapas-DAI/manage.py runserver 0.0.0.0:80')
+
+########   Borramos la aplicacion
+def borrar():
+    run('sudo rm -r /root/bares-y-tapas-DAI')
